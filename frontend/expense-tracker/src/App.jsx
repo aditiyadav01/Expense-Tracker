955d9574b09acc01e70b933f4d5234b1572d27aa
aditiyadav01 <yadavaditi0501@gmail.com>
2025-07-09 14:32:53 +0530

    commit the files

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from 'react-router-dom'; 
-import Login from './pages/Auth/login';
-import SignUp from './pages/Auth/SignUp';
-import Home from './pages/Dashboard/Home';
-import Expense from './pages/Dashboard/Expense';
-import Income from './pages/Dashboard/Income';
-import RecentTransactionsPage from './pages/Dashboard/RecentTransactionsPage '  // new 
-import UserProvider from './context/UserContext';
-import { Toaster } from 'react-hot-toast';
-
-
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Expense from "./pages/Dashboard/Expense";
+import Income from "./pages/Dashboard/Income";
+import RecentTransactionsPage from "./pages/Dashboard/RecentTransactionsPage "; // new
+import UserProvider from "./context/UserContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
@@ -22,39 +20,43 @@ const App = () => {
       <div>
         <Router>
           <Routes>
-            <Route path='/' element={<Root/>}/>
-            <Route path='/login' exact element={<Login/>}/>
-            <Route path='/signup' exact element={<SignUp/>}/>
-            <Route path='/dashboard' exact element={<Home/>}/>
-            <Route path='/expense' exact element={<Expense/>}/>
-            <Route path='/income' exact element={<Income/>}/>
-            <Route path='/transactions' exact element={<RecentTransactionsPage />}/>  {/* new */}
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<SignUp />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/expense" exact element={<Expense />} />
+            <Route path="/income" exact element={<Income />} />
+            <Route
+              path="/transactions"
+              exact
+              element={<RecentTransactionsPage />}
+            />{" "}
+            {/* new */}
           </Routes>
         </Router>
       </div>
       <Toaster
         toastOptions={{
-          className:"",
-          style:{
-            fontSize:'13px'
+          className: "",
+          style: {
+            fontSize: "13px",
           },
         }}
       />
     </UserProvider>
-  )
-}
+  );
+};
 
 export default App;
 
-const Root=()=>{
+const Root = () => {
   // check if token exists in lovalstorage
   const isAuthenticated = !!localStorage.getItem("token");
 
-  //Redirect to dashboard if authenticated, otherwise to login 
+  //Redirect to dashboard if authenticated, otherwise to login
   return isAuthenticated ? (
-    <Navigate to="/dashboard"/>
-  ):(
-    <Navigate to ="/login"/>
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
   );
-
 };
